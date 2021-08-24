99bd65ce811c1291ff04394e1100fe38d77d9b61
wenwl <wwllong18@163.com>
2021-08-25 02:46:46 +0800

    feat：增加熔断器-Netflix Hystrix示例代码

--- hello-spring-cloud-netflix/hello-spring-cloud-netflix-consumer-ribbon/src/main/java/hello/spring/cloud/netflix/consumer/ribbon/RibbonConsumerApplication.java
+++ hello-spring-cloud-netflix/hello-spring-cloud-netflix-consumer-ribbon/src/main/java/hello/spring/cloud/netflix/consumer/ribbon/RibbonConsumerApplication.java
@@ -3,9 +3,11 @@ package hello.spring.cloud.netflix.consumer.ribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class RibbonConsumerApplication {
 
     public static void main(String[] args) {
--- hello-spring-cloud-netflix/hello-spring-cloud-netflix-consumer-ribbon/src/main/java/hello/spring/cloud/netflix/consumer/ribbon/service/ProviderService.java
+++ hello-spring-cloud-netflix/hello-spring-cloud-netflix-consumer-ribbon/src/main/java/hello/spring/cloud/netflix/consumer/ribbon/service/ProviderService.java
@@ -1,5 +1,6 @@
 package hello.spring.cloud.netflix.consumer.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +11,13 @@ public class ProviderService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public String sayHi(String message) {
-        return restTemplate.getForObject("http://hello-spring-cloud-netflix-provider/hi?msg=" + message, String.class);
+    @HystrixCommand(fallbackMethod = "sayHiError")
+    public String sayHi(String msg) {
+        return restTemplate.getForObject("http://hello-spring-cloud-netflix-provider/hi?msg=" + msg, String.class);
+    }
+
+    public String sayHiError(String msg){
+        return "Hi，your message is : " + msg + " but request error.";
     }
 
 }
