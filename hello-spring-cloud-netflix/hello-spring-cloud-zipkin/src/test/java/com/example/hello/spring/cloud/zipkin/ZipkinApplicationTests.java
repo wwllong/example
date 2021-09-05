078e66e9cd87fe234b91c817fce757e0bf35c3c3
wenwl <wwllong18@163.com>
2021-09-05 11:42:41 +0800

    feat：Spring Cloud 集成 ZipKin 示例

---
+++ hello-spring-cloud-netflix/hello-spring-cloud-zipkin/src/test/java/com/example/hello/spring/cloud/zipkin/ZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello.spring.cloud.zipkin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
