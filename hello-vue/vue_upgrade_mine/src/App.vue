a2d8df57961a0ea94e08df872801998c84cc5823
wenwl <wwllong18@163.com>
2025-03-23 23:46:44 +0800

    feat: learn vue ref

--- hello-vue/vue_upgrade_mine/src/App.vue
+++ hello-vue/vue_upgrade_mine/src/App.vue
@@ -1,21 +1,29 @@
 <template>
-	<div>
-		<School></School>
-		<School/>
-		<Student></Student>
-	</div>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button ref="btn" @click="showDOM">点我输出上方的DOM元素</button>
+    <School ref="sch"/>
+  </div>
 </template>
 
 <script>
-	//引入组件
-	import School from './components/School'
-	import Student from './components/Student'
+//引入School组件
+import School from './components/School'
 
-	export default {
-		name:'App',
-		components:{
-			School,
-			Student
-		}
-	}
+export default {
+  name:'App',
+  components:{School},
+  data() {
+    return {
+      msg: 'Hello Vue！'
+    }
+  },
+  methods: {
+    showDOM(){
+      console.log(this.$refs.title) //真实DOM元素
+      console.log(this.$refs.btn) //真实DOM元素
+      console.log(this.$refs.sch) //School组件的实例对象（vc）
+    }
+  },
+}
 </script>
