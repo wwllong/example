af22b3b94521b4761010a2e309123334fac18190
wenwl <wwllong18@163.com>
2025-10-26 23:34:49 +0800

    feat[vue]: github搜索案例

---
+++ hello-vue/vue_upgrade_mine/src/components/List.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="row">
+    <div v-show="userResp.users.length" class="card" v-for="user in userResp.users" :key="user.login">
+      <a :href="user.html_url" target="_blank">
+        <img :src="user.avatar_url" style='width: 100px'/>
+      </a>
+      <p class="card-text">{{user.login}}</p>
+    </div>
+    <h1 v-show="userResp.isFirst">欢迎使用</h1>
+    <h1 v-show="userResp.isLoading">加载中...</h1>
+    <h1 v-show="userResp.errMsg" style="color: red">{{userResp.errMsg}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'List',
+    data() {
+      return {
+        userResp: {
+          isFirst: true,
+          isLoading: false,
+          errMsg: '',
+          users: []
+        }
+      }
+    },
+    mounted() {
+      this.$bus.$on('getUsers', (userResp) => {
+        console.log('List组件接收到数据了', userResp)
+        this.userResp = {...this.userResp, ...userResp}
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .album {
+    min-height: 50rem; /* Can be removed; just added for demo purposes */
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+    background-color: #f7f7f7;
+  }
+
+  .card {
+    float: left;
+    width: 33.333%;
+    padding: .75rem;
+    margin-bottom: 2rem;
+    border: 1px solid #efefef;
+    text-align: center;
+  }
+
+  .card > img {
+    margin-bottom: .75rem;
+    border-radius: 100px;
+  }
+
+  .card-text {
+    font-size: 85%;
+  }
+</style>
